2d544bf1251c6690aee2dde9370f65549fcca1e1
Clement Sauvage <clement.sauvage@hei.yncrea.fr>
2021-08-05 01:31:13 +0200

    Rename my_fizzbuzz local variable

--- rest_fizzbuzz/utils.py
+++ rest_fizzbuzz/utils.py
@@ -2,18 +2,18 @@ from typing import List
 
 
 def my_fizzbuzz(int1: int, int2: int, limit: int, string1: str, string2: str) -> List[str]:
-    response = []
+    fizzbuzz = []
     for i in range(1, limit + 1):
         if i % int1 == 0 and i % int2 == 0:
-            response.append(string1 + string2)
+            fizzbuzz.append(string1 + string2)
             continue
         elif i % int1 == 0:
-            response.append(string1)
+            fizzbuzz.append(string1)
             continue
         elif i % int2 == 0:
-            response.append(string2)
+            fizzbuzz.append(string2)
             continue
         else:
-            response.append(str(i))
+            fizzbuzz.append(str(i))
 
-    return response
+    return fizzbuzz
